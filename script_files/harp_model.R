eb3e6347029c83efeeda351a8f30b94e74e5b882
Olivia Somhegyi <oliviasomhegyi@bren.ucsb.edu>
2023-10-30 22:09:32 -0700

    Made a some progress for running nls for one population. Confused at how to use purrr for nls. Will research tomorrow.

--- script_files/harp_model.R
+++ script_files/harp_model.R
@@ -1,5 +1,14 @@
 ## creating new population model
 
+## filter for a single population to start
+alsea_coho <- coho %>% 
+  select(year, alsea)
+
+## initial visualization
+ggplot(data=alsea_coho,aes(x=year,y=alsea))+
+  geom_point(size=2,color="black")+
+  theme_minimal()
+
 
 ## s_t+1 = ((p_i)(S_t_i))/(1+ (S_t_i/c_i))
 
@@ -10,15 +19,51 @@ calculate_spawners <- function(abundance, prod, capacity){
 
 ## guesses for nls
 
-guess_vec = c(sample(1:16000, 21))
+guess_vec = c(sample(1:16000, 21)) ## Question from Olivia: why choose 1:16000?
 
 ## running nls with the nls wrapper function
 
-run_nls = nls(abundance~calculate_spawners(abundance, prod, capacity),
-              data = coho,
+# copied original nls function that Alicia created (copied down below) and ran it with just the Alsea data as a test
+run_nls = nls(alsea~calculate_spawners(alsea, prod, capacity),
+              data = alsea_coho,
               start = list(prod = guess_vec[1],
                            capacity = guess_vec[2]),
               trace = TRUE)
+
+# run_nls = nls(abundance~calculate_spawners(abundance, prod, capacity),
+#               data = coho,
+#               start = list(prod = guess_vec[1],
+#                            capacity = guess_vec[2]),
+#               trace = TRUE)
+
+## show how the model fits the data by making a prediction based on the data
+nls_predict<-alsea_coho %>% 
+  mutate(predict=predict(run_nls,newdata=.))
+
+ggplot(data=nls_predict)+
+  geom_point(aes(x=year,y=alsea))+
+  geom_path(aes(x=year,y=predict),color='red')+
+  theme_minimal()
+
+## add control nls model to compare
+control_nls=nls(alsea~calculate_spawners(alsea, prod, capacity),
+                data = alsea_coho,
+                start = list(prod = guess_vec[1],
+                             capacity = guess_vec[2]),
+                control = nls.control(tol = 2.1e-9,minFactor=1e-10,warnOnly = TRUE)) #these values are the same from lab 4, was not sure how to edit these values
+
+## tables of nls values for control and first run. The productivity and capacity are negative... shouldn't they be positive?
+broom::tidy(control_nls) %>% 
+  kable(caption = "Control NLS") %>% 
+  kable_classic()
+
+broom::tidy(run_nls) %>% 
+  kable(caption = "Original NLS") %>% 
+  kable_classic()
+
+
+## run with purrr
+
 
 ## Gauss-Newton
 ## Use AIC or BIC or cross fold validation to compare different model fits
