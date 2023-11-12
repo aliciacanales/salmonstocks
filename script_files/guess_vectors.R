045f7807f2487dcdd23ce0e7759981d3385b639d
aliciacanales <canalesalicia06@gmail.com>
2023-11-12 15:35:39 -0800

    cleaning up guess_vector script

--- script_files/guess_vectors.R
+++ script_files/guess_vectors.R
@@ -1,6 +1,8 @@
 ## all 21 populations 
 
-## cleaning data to generate reciprocal form of recruits and abundance 
+## Should we move lines 5-14 to the nls script?
+
+## cleaning data to generate reciprocal form of recruits and abundance
 coho_recruits <- coho %>% 
 pivot_longer(cols = 2:22,
              names_to = 'population',
@@ -12,18 +14,17 @@ pivot_longer(cols = 2:22,
   mutate(recruits_flip = 1/ recruits,
          abundance_flip = 1/ abundance)
 
+
+###################################################################################################################################################################################
+## Don't think we need any of this
+
 ## calculating pre_hat coefficients 
-pre_hat <- coho_recruits %>% 
-  group_by(population) %>% 
-  summarize(intercept = coefficients(lm(recruits_flip ~ abundance_flip))[1], 
-            coefficient = coefficients(lm(recruits_flip ~ abundance_flip))[2]) %>%
-  mutate(p_hat = 1/coefficient, ## calculating p_hat and c_hat for all 21 populations
-         c_hat = 1/ (intercept * (1/ coefficient)))
-
-pre_hat
-
-## calculating p_hat and c_hat for all 21 populations
-# pre_hat <- pre_hat %>% 
-#   mutate(p_hat = 1/coefficient,
+# pre_hat <- coho_recruits %>% 
+#   group_by(population) %>% 
+#   summarize(intercept = coefficients(lm(recruits_flip ~ abundance_flip))[1], 
+#             coefficient = coefficients(lm(recruits_flip ~ abundance_flip))[2]) %>%
+#   mutate(p_hat = 1/coefficient, ## calculating p_hat and c_hat for all 21 populations
 #          c_hat = 1/ (intercept * (1/ coefficient)))
-# coho_guess
+# 
+# pre_hat
+
