6cdb8b2769a3d9fa088a2efe432e6cae34afa66c
Olivia Somhegyi <oliviasomhegyi@bren.ucsb.edu>
2023-11-26 10:52:25 -0800

    Added a few notes. Thinking about how to pass through all potential weight allocations into equation

--- script_files/nls.R
+++ script_files/nls.R
@@ -34,30 +34,30 @@ equilibrium_all <- coho_recruits %>%
 ### Code up the model for a hypothetical scenario to create framework:
 ## $10 of investment leads to a 1% increase in p and c
 
-calculate_return_investment <- function(p, c, alpha, beta, weight){
-  y = (((p*alpha(weight))-1)*(c*beta(weight)))
-  return(y)
-}
+# calculate_return_investment <- function(p, c, alpha, beta, weight){
+#   y = (((p*alpha(weight))-1)*(c*beta(weight)))
+#   return(y)
+# }
 
 ## Big Function - can we instead use the simpler function below (s_fun_new) to calculate s? - OS
-s_fun <- function(population, weight, p_change, c_change){
-  eq <- (p_hat(weight, p_change) - 1) * c_hat(weight,c_change)
-  return(eq)
-}
+# s_fun <- function(population, weight, p_change, c_change){
+#   eq <- (p_hat(weight, p_change) - 1) * c_hat(weight,c_change)
+#   return(eq)
+# }
 
-s_fun_new <- function(delta_p, delta_c){
+s_fun <- function(delta_p, delta_c){
   s_invest <- (delta_p-1)*delta_c
   return(s_invest)
 }
 
 ## Calculate change in p_hat which will be put into "big equation" (This means: P is a function of Alpha(passage) * w(i) + p_hat (from nls))
 p_hat_fun <- function(p_hat,p_change, weight){ 
-  p <- (weight * p_change) + p_hat
+  p <- ((weight/10) * p_change) + p_hat # should weight be "w/10" so that we multiply by the number of investments and not the dollar amount? - OS
 }
 
 ## Calculate change in c_hat which will be put into "big equation" (This means: c is a function of Beta(passage) * w(i) + c_hat (from nls))
 c_hat_fun <- function(c_hat, c_change, weight){ 
-  c <- (weight * c_change) + c_hat
+  c <- ((weight/10) * c_change) + c_hat
 }
 
 ## bringing out the coefficients into separate columns and applying a $10 investment which will have a .01 increase
@@ -70,9 +70,21 @@ new_stock <- equilibrium_all %>%
          c_change = .01,
          delta_p = map(p_hat, ~p_hat_fun(.x,p_change, weight)),
          delta_c = map(c_hat, ~c_hat_fun(.x,c_change, weight)),
-         s_baseline = pmap(list(p_hat,c_hat),s_fun_new), #calculate s before investment to compare with s after investment
-         s_invest = pmap(c(delta_p,delta_c),s_fun_new)) # calculate s after investment using new p and c
-## I created a new function "s_fun_new", but feel free to change naming -- called it 'new' just to be clear that is is different from the other - OS
+         s_baseline = pmap(list(p_hat,c_hat),s_fun), #calculate s before investment to compare with s after investment
+         s_invest = pmap(c(delta_p,delta_c),s_fun)) # calculate s after investment using new p and c
+
+# ## test with weight allocation of $30 to verify all is running correctly
+# new_stock_30 <- equilibrium_all %>% 
+#   mutate(p_hat = map_dbl(coeff, ~.[['p_hat']]),
+#          c_hat = map_dbl(coeff, ~.[['c_hat']])) %>%
+#   select(population, p_hat, c_hat) %>% 
+#   mutate(weight = 30,
+#          p_change = .01,
+#          c_change = .01,
+#          delta_p = map(p_hat, ~p_hat_fun(.x,p_change, weight)),
+#          delta_c = map(c_hat, ~c_hat_fun(.x,c_change, weight)),
+#          s_baseline = pmap(list(p_hat,c_hat),s_fun), #calculate s before investment to compare with s after investment
+#          s_invest = pmap(c(delta_p,delta_c),s_fun))
 
 ## calculate difference in investment to visualize
 new_stock <- new_stock %>% 
@@ -82,6 +94,9 @@ new_stock <- new_stock %>%
 
 new_stock2 <- pmap(new_stock, ~s_fun(..1, ..3, ..4, ..5))
 
+## Create a vector of weight allocation options
+w <- seq(10,1000, by=10)
+w
 
 
 #############
