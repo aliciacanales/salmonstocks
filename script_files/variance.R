9a172762591266239dfca2ffbe56fa413e693b3a
Olivia Somhegyi <oliviasomhegyi@bren.ucsb.edu>
2024-01-17 05:22:53 -0800

    Cleaned up code.

--- script_files/variance.R
+++ script_files/variance.R
@@ -4,63 +4,47 @@ library(tidyverse)
 library(LaplacesDemon)
 library(nloptr)
 
-#Generate 1000 portfolio weights for all 20 streams
+
 n=100
-#raw<-rdirichlet(n,rep(1,20)) #Replace 20 with relative reference e.g. ncol(abundance_data)
+abundance_data <- coho[2:22] # raw population abundance data
+abundance_data <- data.frame(abundance_data[-18]) # remove Tahkenitch
+raw<-rdirichlet(n,rep(1,ncol(abundance_data))) #Generate 100 portfolio weights for all 20 streams
+rounded<-round(raw,2) # round to two decimals
 
-abundance_data <- coho[2:22]
-abundance_data <- data.frame(abundance_data[-18])
-
-raw<-rdirichlet(n,rep(1,ncol(abundance_data)))
-
-# I don't like the really small 0s and decimals. What is a budget of 0.0002%?
-rounded<-round(raw,2)
-
-#But now I need to make sure the weights still add up to 1. Take the difference at each element then sum. Add the sum to highest weighted stream
-
-diff<-as.data.frame(raw-rounded) %>% 
-  mutate(sum=rowSums(across(everything())))
+# Need to make sure the weights still add up to 1
+diff<-as.data.frame(raw-rounded) %>% # Take the difference at each element
+  mutate(sum=rowSums(across(everything()))) # Sum across rows. Add the sum to highest weighted stream
 
 for(i in 1:nrow(abundance_data)){
   rounded[i,which.max(rounded[i,])]<-rounded[i,which.max(rounded[i,])]+diff$sum[i]
 }
 
-
-# Check to make sure weights still add up to 1 for rounded dataset
+# Check to make sure weights still add up to 1 for rounded dataframe
 check<-as.data.frame(rounded) %>% 
   mutate(sum=rowSums(across(everything()))) %>% ## Does not all add up to 1
   filter(sum==1.00) %>% ## filter for only portfolios that sum to 1
   select(-sum) ## remove sum column
 
-# I think we should always run a few portfolios that are the same each time. Mainly, give the entire budget to each stream and see how it responds.
+# Add in QC: run a few portfolios that are the same each time, so we can make sure it is running correctly. Give the entire budget to each stream and see how it responds
+full<-diag(ncol(abundance_data)) # create set portfolios
 
-#full<-diag(20) #Replace 20 with relative reference e.g. ncol(abundance_data)
-full<-diag(ncol(abundance_data))
 # add set simulations with random weights
-
 #weights<-rbind(check,full) ## commenting this out just for now to run it with fewer portfolios while we get function running
 weights <- check
 
 # From here you can make the "weights" a dataframe add the stream names in each column and anything else to make it ready to pass into purrr map
 
 colnames(weights) <- names(abundance_data) ## Assign column names from original dataframe
-weights_t <- t(weights) ## transforming data before unlist()
+#weights_t <- t(weights) ## transforming data before unlist()
 
 grid_list<-split(weights,seq(nrow(weights)))
-grid_list_2<-split(weights_t,seq(ncol(weights_t))) ## grid_list column instead of row
-  ## need to make it a list to pass through. Include the guess vectors in this list?
+#grid_list_2<-split(weights_t,seq(ncol(weights_t))) ## grid_list column instead of row
+# need to make it a list to pass through
 
-
-##### create new max function that incorporate p_hat_fun, c_hat_fun, s_fun, and variance
-
-## The starting dataframe should have the following columns: population, p_hat, c_hat, p_change, c_change, var
-## Then insert weight matrix for multiple portfolios
-
-## I was able to get some columns to work - Alicia
-## Yayyyy!!! :)
+## Create max function that incorporates p_hat_fun, c_hat_fun, s_fun, and var. Then run altogether using purrr
 
 max_fcn <- function(weight){
-  weight=weights_t %>% unlist()
+  weight=weights %>% unlist()
  
 invest <- equilibrium_all %>% 
     mutate(p_hat = map_dbl(coeff, ~.[['p_hat']]),
@@ -78,19 +62,21 @@ invest <- equilibrium_all %>%
   var_invest <- var * (s_invest^2)
 
   return(round(data.frame(delta_p, delta_c, s_invest, var_invest),3))
-                          # ,var_invest=out$objective),5))
 }
 
-try=map_df(.x=grid_list_2,~max_fcn(.x))
+try=map_df(.x=grid_list,~max_fcn(.x))
+
 ## Need to find a way to bind each of the rows by number (data is holding the portfolio number, which is good, but we will ultimately want to sum by portfolio to get total portfolio return and variance)
 
-#####
 
 
 
 
 
 
+
+
+######## misc old code below
 
 
 max_fcn<-function(x){
