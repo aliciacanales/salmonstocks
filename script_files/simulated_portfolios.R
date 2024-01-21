17b33f7dab9dba4a0d61bb104bdff81e9cd44c84
Olivia Somhegyi <oliviasomhegyi@bren.ucsb.edu>
2024-01-20 19:21:43 -0800

    Getting closer!! Think I figured out how to link weights to budget and calculate p_invest using it. Final step in this is to link monetary investment to improvement to beta passage. (i.e. multiple money invested to cost to remove barrier beucase right now we are multiplying money invested by beta_passage - gotta multiple same "units") - also I think we should link all barriers into one so that we are only dealing with one cost. Alternative would be to separate barrier types and run analysis separately.

--- script_files/simulated_portfolios.R
+++ script_files/simulated_portfolios.R
@@ -45,7 +45,7 @@ p_hat_temp = equilibrium_all %>%
 ## Import beta dataframe the dataframe should have the following columns: population, beta_passage
 ### For now, create temp dataframe to get function running, replace when b_passage for each population is ready
 b_passage_temp <- cbind(p_hat_temp$population) %>% 
-  data.frame(b_passage=c(.125, .1, .005, .2, .10, .12, .015, .1, .04, .08, .095, .13, .1, .045, .05, .11, .2, .005, .125, .125)) %>%
+  data.frame(b_passage=c(.000125, .0001, .000005, .0002, .00010, .00012, .000015, .0001, .00004, .00008, .000095, .00013, .0001, .000045, .00005, .00011, .0002, .000005, .000125, .000125)) %>%
   rename(population = 1)
 
 
@@ -68,14 +68,14 @@ z_b_df <- b_passage_temp %>%
 ## we need to have an intermediate between these two steps where our weights come in to impact b_invest
 
 ########## create function to see impact on productivity after investment
-p_invest_fcn <- function(z,b_invest){
-  p_invest = z * b_invest
-  return(p_invest)
+p_invest_fcn <- function(z,b_passage,weight){
+  p_invest = z * (b_passage * (weight * 1000000)) #replace 1000000 with defined budget, but doing manually first to check if it works
+  return(p_invest) # this is wrong right now because we are multiplying money by passage, but we need to multiply money by investment in increasing passage so that it is money into money
 }
 
 ########## create function to see impact on capacity after investment
-c_invest_fcn <- function(z,b_invest){
-  c_invest = z * b_invest
+c_invest_fcn <- function(z,b_passage){
+  c_invest = z * (b_passage)
   return(c_invest)
 }
 
@@ -85,6 +85,11 @@ p_temp <- z_b_df %>% # using fake data right now but its working, so cool!
     p_invest = pmap_dbl(list(z,b_passage),c_invest_fcn) # this b_passage should be b_invest (update when we have real data and weights added in)
   )
 
+
+####### to link weight allocations to budget, we need to:
+# define budgets (make it a dataframe)
+# weight * budget
+# multiply this by b_baseline? or is there a better way to link it? Q for Nathan and Tamma
 
 ## Simulating portfolios 
 max_fcn <- function(weight){
@@ -97,23 +102,24 @@ max_fcn <- function(weight){
            c_hat = map_dbl(coeff, ~.[['c_hat']])) %>%
     select(population, p_hat, c_hat) 
   
-  p_change <- pmap_dbl(list(z_b_df$z,z_b_df$b_passage),p_invest_fcn) ## this works! We should manually check this tho
-  c_change <- pmap_dbl(list(z_b_df$z,z_b_df$b_passage),c_invest_fcn)
+  p_invest <- pmap_dbl(list(z_b_df$z,z_b_df$b_passage, weight),p_invest_fcn) ## this works! We should manually check this tho
+  c_invest <- pmap_dbl(list(z_b_df$z,z_b_df$b_passage),c_invest_fcn)
   #impact_p <- p_change(b_passage = .555) ## this function is working. if we have multiple b_passages how can we manually change the values. another list? another purrr??? yikes
   #c_change = .001
   var <- sapply(coho[2:22], var)
   var_rm<-var[-18]
            
-  delta_p <- baseline$p_hat * (1 + p_change * weight) #can we change delta_p to p_invest?
-  delta_c <- baseline$c_hat * (1 + c_change * weight) #can we change delta_c to c_invest?
-  s_invest <- ((delta_p - 1) * delta_c)
+  #delta_p <- baseline$p_hat * (1 + p_change * weight) #can we change delta_p to p_invest?
+  #delta_c <- baseline$c_hat * (1 + c_change * weight) #can we change delta_c to c_invest?
+  s_invest <- ((p_invest - 1) * c_invest)
   s_baseline <- ((baseline$p_hat -1) * baseline$c_hat)
   var_invest <- var_rm * (s_invest^2)
   esu_returns <- sum(s_invest)
+  esu_baseline <- sum(s_baseline)
   esu_var <- sum(var_invest)
 
 
-  return(round(data.frame(esu_returns, esu_var),3))
+  return(round(data.frame(esu_returns,esu_baseline, esu_var),3))
 }
 
 portfolios = map_df(.x=grid_list,~max_fcn(.x))
