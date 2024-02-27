bc04e4ff23062e36b30fc5085b7bdd989da302d1
aliciacanales <canalesalicia06@gmail.com>
2024-02-26 21:33:25 -0800

    changed name of big function and output name

--- script_files/messy_code_in_progress.R
+++ script_files/messy_code_in_progress.R
@@ -47,7 +47,7 @@ cov_matrix <- coho[2:22] %>%
 cov_matrix <- cov(cov_matrix)
 
 
-test_max_fcn <- function(weight){
+optimize_fcn <- function(weight){
   weight=weight %>% unlist()
   output1 <- pmap_dbl(list(budget, weight),budget_allocated_fcn) 
   output2 <- (pmap_dbl(list(output1),while_fcn)-1) # check to see if this is being transformed
@@ -100,7 +100,7 @@ test_max_fcn <- function(weight){
 }
 
 
-test = map_df(.x=grid_list,~test_max_fcn(.x)) %>%
+portfolios = map_df(.x=grid_list,~optimize_fcn(.x)) %>%
 
   arrange(esu_returns_invest) # order by returns from investment
 
@@ -127,7 +127,6 @@ my_plot <- ggplot(temp, aes(x = esu_var_invest, y = esu_returns_invest)) +
   # xend = 3.892000e+17, yend = 211781.8,
   # colour = 'red', curvature = -.3) +
   geom_point(data = baseline_point, aes(x, y), color = "black", size = 3) +
-<<<<<<< HEAD
   #annotate("segment",
            #x = 1.5e+28, xend = 3.14e+27 , ## this controls how long the arrow is
            #y = 187118.2, yend = 187118.2, ## controls where the tip of the arrow ends
