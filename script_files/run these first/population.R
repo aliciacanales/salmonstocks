1f05bd62693d9ff746513e57ff6930b98fccfbbe
Olivia Somhegyi <oliviasomhegyi@bren.ucsb.edu>
2024-03-06 20:46:14 -0800

    remove Sizes and Tahkenitch from coho data

--- script_files/run these first/population.R
+++ script_files/run these first/population.R
@@ -14,7 +14,9 @@ library(broom)
 
 coho <- readxl::read_xlsx(here('data', 'OC Coho Abundance.xlsx')) %>%
   clean_names() %>% 
-  distinct()
+  distinct() %>% 
+  select(-tahkenitch) %>% 
+  select(-sixes)
 
 coho_yr <- coho %>%
   mutate(date = lubridate::mdy(year)) #Convert to date for tibble, this didn't work tho so we need to revisit this.
